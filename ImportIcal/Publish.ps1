0264648b928650baeac6d3870183a55d7fca3a84
Craig Chamberlain <craig.chamberlain11@gmail.com>
2025-02-02 21:18:16 +0000

    Fix path

--- ImportIcal/Publish.ps1
+++ ImportIcal/Publish.ps1
@@ -1,5 +1,5 @@
 ﻿dotnet publish -c Release
-cd ".\Ical\bin\Release\net7.0\publish"
+cd ".\ImportIcal\bin\Release\net7.0\publish"
 Publish-Module -Name .\ImportIcal.psd1 -NuGetApiKey (Get-Secret NuGetApiKey -AsPlainText )
 
 
