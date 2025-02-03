8d5c1e782515e7b531f952b1329c8e7651c9f0d8
Craig Chamberlain <craig.chamberlain11@gmail.com>
2025-02-03 15:09:02 +0000

    Make tests easier to rerun.

--- ImportIcal/Test.ps1
+++ ImportIcal/Test.ps1
@@ -1,4 +1,10 @@
-﻿dotnet publish "$PSScriptRoot" -c Release
+﻿param(
+	[switch] $JustTest
+)
+
+if  (!$JustTest){
+dotnet publish "$PSScriptRoot" -c Release
+}
 Import-Module "$PSScriptRoot/bin/Release/net7.0/publish/ImportIcal.psd1"
 
 Invoke-Pester "$PSScriptRoot/tests"
