c4ce3b8785f8d081c9684d14fd480c26d03628ce
Craig Chamberlain <craig.chamberlain11@gmail.com>
2025-02-03 15:49:39 +0000

    Add Call to action when update is available.

--- ImportIcal/ImportScript.ps1
+++ ImportIcal/ImportScript.ps1
@@ -1,2 +1,14 @@
-Write-Warning("Please check for updates regularly, this is in early development and I will be adding features regularly.`r`nThe API is prone to change at this stage.")
-# TODO Write-Warning if newer version is available see pnpPowershell
+$local = [System.Version](Get-ItemProperty "$PSScriptRoot/ImportIcal.dll").versioninfo.ProductVersionRaw
+
+$online = 
+	Find-Module ImportIcal | 
+	Select-Object -First 1 -ExpandProperty Version |
+	ForEach-Object {
+		 [System.Version]::new($_)
+	}
+if($local -lt $online){
+	Write-Warning("Version $online available.  Please update: Update-Module ImportIcal")
+
+}elseif($null -eq $online){
+	Write-Warning("Please check for updates regularly, this is in early development and I will be adding features regularly.`r`nThe API is prone to change at this stage.")
+}
