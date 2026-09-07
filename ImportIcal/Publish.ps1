6af550eaf3632c8113ade9ab827ccf5e012c96bc
Craig <craig.chamberlain11@gmail.com>
2026-09-07 12:15:49 +0100

    Update version

--- ImportIcal/Publish.ps1
+++ ImportIcal/Publish.ps1
@@ -1,5 +1,5 @@
 ﻿dotnet publish -c Release
-Push-Location ".\ImportIcal\bin\Release\net7.0\publish"
+Push-Location ".\bin\Release\net10.0\publish"
 Publish-Module -Name .\ImportIcal.psd1 -NuGetApiKey (Get-Secret NuGetApiKey -AsPlainText ) -AllowPrerelease
 Pop-Location
 
